4d592abbbef3039a82aa27d2d78d16d585bfef7e
Cristian Parada <cristianoronaldo8k@gmail.com>
2023-03-15 15:39:59 -0500

    Initial commit sem1

---
+++ app/src/main/java/com/example/parada_cabrera/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.parada_cabrera;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Producto> listaPrincipalProductos;
+    private RecyclerView rvListadoProductos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cargarDatos();
+
+        rvListadoProductos = findViewById(R.id.rv_listado_productos);
+
+        AdaptadorPersonalizado miAdaptador = new AdaptadorPersonalizado(listaPrincipalProductos);
+
+        miAdaptador.setOnItemClickListener(new AdaptadorPersonalizado.OnItemClickListener() {
+            @Override
+            public void onItemClick(Producto miProducto, int posicion) {
+                Toast.makeText(MainActivity.this, "HICE CLICK DESDE ACTIVITY" + miProducto.getNombre(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemBtnEliminaClick(Producto miProducto, int posicion) {
+                listaPrincipalProductos.remove(posicion);
+                miAdaptador.setListadoInformacion(listaPrincipalProductos);
+            }
+        });
+
+        rvListadoProductos.setAdapter(miAdaptador);
+        rvListadoProductos.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void cargarDatos(){
+        Producto producto1 = new Producto();
+        producto1.setNombre("Computador HP");
+        producto1.setPrecio(8000.0);
+        producto1.setUrlImagen("https://e7.pngegg.com/pngimages/565/899/png-clipart-laptop-hewlett-packard-hp-pavilion-15-cd000-series-intel-core-i5-laptop-gadget-electronics.png");
+
+        Producto producto2 = new Producto("Teclado Dell",
+                800.00,
+                "https://e7.pngegg.com/pngimages/454/792/png-clipart-computer-keyboard-computer-mouse-keyboard-protectors-bluetooth-dell-computer-mouse-electronics-computer-keyboard.png");
+        Producto producto3 = new Producto("Mouse",
+                5000.0,
+                "https://w7.pngwing.com/pngs/303/196/png-transparent-computer-mouse-logitech-g402-hyperion-fury-amazon-com-optical-mouse-computer-mouse-electronics-computer-video-game.png");
+
+        listaPrincipalProductos = new ArrayList<>();
+
+        listaPrincipalProductos.add(producto1);
+        listaPrincipalProductos.add(producto2);
+        listaPrincipalProductos.add(producto3);
+    }
+}
